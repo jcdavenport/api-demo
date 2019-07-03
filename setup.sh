31abbcb349effe0d7c5107788bd8d4bd6e775040
Josh <jdavenport@protonmail.com>
2019-07-03 16:00:41 -0500

    updated project

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@
 # Install docker-compose:
 sudo apt update && sudo apt install -y docker-compose 
 
-# create a new test network(ip: 172.19.0.0/16):
-docker network create --driver=bridge test-net
+# change owner permissions for warfiles:
+sudo chown 1001:root warfiles/* && sudo chmod 644 warfiles/*
 
 # run demo containers in the background:
-docker-compose up -d && echo done...
+docker-compose up -d && sleep 5
 
 # Copy war service file into Tomcat:
 docker cp warfiles/SOAP_Customer.war customers:/app
+docker cp warfiles/SOAP_Product.war products:/app
 
-
+echo done...
